c92a519aea1f20f9b58923fd2cfed8c6410ee911
drew <Frazia@uw.edu>
2021-11-23 09:31:04 -0800

    added react functionality for card list on main page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {CardList} from "HomePage";
+
+
+export function App(props) {
+
+
+    return (
+        <div>
+        <h1>Today's Habits</h1>
+            <CardList />
+        
+          
+        </div>
+    )
+
+}
