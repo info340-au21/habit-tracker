5eb65d43a0ede21d90d54b95cf23f3f6d4744407
augene <ajp824@uw.edu>
2021-11-24 21:12:13 -0800

    Fixed footer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -21,10 +21,10 @@ export function NavBar(props) {
 
 export function Footer(props) {
     return (
-        <footer className="text-center text-lg-start bg-light text-muted">
+        <footer className="page-footer font-small bg-dark pt-4">
             <div className="container">
                 <p>&copy;Augene Pak, John Danielsson, Andrew Frazier, Luka Marceta</p>
-                <p>Contact information: ajp824@uw.edu, frazia@uw.edu, lukamarc@uw.edu</p>
+                <p>Contact information: ajp824@uw.edu, frazia@uw.edu, lukamarc@uw.edu, johnaugustd@gmail.com</p>
             </div>
         </footer>
     );
