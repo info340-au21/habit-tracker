aace0a695690f7974fc3f33c03c63854c7daeff4
Augene Pak <augene.pak@gmail.com>
2021-12-12 11:53:07 -0800

    fixed router

--- src/components/About.js
+++ src/components/About.js
@@ -1,87 +1,98 @@
-import React from 'react';
-{/* Remove the <br>s. */}
-export function About(props) {
-    return (
+import React from "react";
+{
+  /* Remove the <br>s. */
+}
+
+function About() {
+  return (
+    <div>
+      <div className="container justify-content-center">
         <div>
-            <div className="container justify-content-center">
-            <div>
-                <h1>The 4 parts of a Habit are the Cue, Craving, Response, and Reward.</h1>
-                    <h2>Cue</h2>
-                    <p>
-                        The Cue is what makes you think of doing the Habit. It's a piece of information
-                        that predicts a reward. For example, if your stomach rumbles, you may think
-                        about eating food.
-                    </p>
-                    <h2>Craving</h2>
-                    <p>
-                        The Craving is what you fundamentally desire. It's not always obvious.
-                        You don't want to go on Netflix, you want to be entertained. You don't want to brush and
-                        floss your teeth, you want the feeling of a clean mouth. You don't want to exercise,
-                        you want the feeling of more energy and attraction from the opposite sex.
-                    </p>
-                    <h2>Response</h2>
-                    <p>
-                        The response is simply the physical action associated with the habit. brushing and flossing,
-                        calling a friend every month, lifting weights, or reading 10 pages of a self-improvement book
-                        every day.
-                    </p>
-                    <h2>Reward</h2>
-                    <p>
-                        The reward is the satiation of the desire (craving). When you brush and floss your teeth, you
-                        are rewarded with the feeling of a clean mouth. Similarly, when you finish lifting weights,
-                        you are rewarded with the feeling of more energy and the satisfaction of having a better looking body.
-                    </p>
-            </div>
-        
-            <div>
+          <h1>
+            The 4 parts of a Habit are the Cue, Craving, Response, and Reward.
+          </h1>
+          <h2>Cue</h2>
+          <p>
+            The Cue is what makes you think of doing the Habit. It's a piece of
+            information that predicts a reward. For example, if your stomach
+            rumbles, you may think about eating food.
+          </p>
+          <h2>Craving</h2>
+          <p>
+            The Craving is what you fundamentally desire. It's not always
+            obvious. You don't want to go on Netflix, you want to be
+            entertained. You don't want to brush and floss your teeth, you want
+            the feeling of a clean mouth. You don't want to exercise, you want
+            the feeling of more energy and attraction from the opposite sex.
+          </p>
+          <h2>Response</h2>
+          <p>
+            The response is simply the physical action associated with the
+            habit. brushing and flossing, calling a friend every month, lifting
+            weights, or reading 10 pages of a self-improvement book every day.
+          </p>
+          <h2>Reward</h2>
+          <p>
+            The reward is the satiation of the desire (craving). When you brush
+            and floss your teeth, you are rewarded with the feeling of a clean
+            mouth. Similarly, when you finish lifting weights, you are rewarded
+            with the feeling of more energy and the satisfaction of having a
+            better looking body.
+          </p>
+        </div>
 
-                <h1>We use 4 metrics when evaluating a Habit: Direction, Impact, Total Impact, and Frequency.</h1>
+        <div>
+          <h1>
+            We use 4 metrics when evaluating a Habit: Direction, Impact, Total
+            Impact, and Frequency.
+          </h1>
 
-                <h2>Direction</h2>
-                <p>
-                    Direction measures whether a habit is good (1), neutral (0), or bad (-1).
-                </p>
+          <h2>Direction</h2>
+          <p>
+            Direction measures whether a habit is good (1), neutral (0), or bad
+            (-1).
+          </p>
 
-                <h2>Impact</h2>
-                <p>
-                    Impact measures, on a scale of 0 to 10, how much of the day is impacted by performing the
-                    habit. The scoring guidelines are as follows: 0 = negligible impact or no impact, 5 = up
-                    to half the day is impacted, 10 = the whole day is impacted. For any value X above 10,
-                    divide X by 10 to get the number of days impacted by the habit.
-                </p>
+          <h2>Impact</h2>
+          <p>
+            Impact measures, on a scale of 0 to 10, how much of the day is
+            impacted by performing the habit. The scoring guidelines are as
+            follows: 0 = negligible impact or no impact, 5 = up to half the day
+            is impacted, 10 = the whole day is impacted. For any value X above
+            10, divide X by 10 to get the number of days impacted by the habit.
+          </p>
 
-                <h2>Total Impact</h2>
-                <p>
-                    Total Impact measures the total effect of a Habit based on its Impact score, whether that is negative, neutral,
-                    or positive.
-                </p>
+          <h2>Total Impact</h2>
+          <p>
+            Total Impact measures the total effect of a Habit based on its
+            Impact score, whether that is negative, neutral, or positive.
+          </p>
 
-                <h2>Frequency</h2>
-                <p>
-                    Frequency measures how often a Habit is being done. The values are: y = yes (done 75%
-                    of the time it should be done), s = sometimes, and n = no (done 25% or less than how often it
-                    should be done).
-                </p>
+          <h2>Frequency</h2>
+          <p>
+            Frequency measures how often a Habit is being done. The values are:
+            y = yes (done 75% of the time it should be done), s = sometimes, and
+            n = no (done 25% or less than how often it should be done).
+          </p>
 
-                <h2>Examples</h2>
+          <h2>Examples</h2>
 
-                <h3>Example 1:</h3>
-                <p>Habit = "Lift weights every day."</p>
-                <p>Direction = 1</p>
-                <p>Impact = 10</p>
-                <p>Total Impact = 1 x 10 = 10</p>
-                <p>Frequency = y</p>
+          <h3>Example 1:</h3>
+          <p>Habit = "Lift weights every day."</p>
+          <p>Direction = 1</p>
+          <p>Impact = 10</p>
+          <p>Total Impact = 1 x 10 = 10</p>
+          <p>Frequency = y</p>
 
-                <h3>Example 2:</h3>
-                <p>Habit = "Eating to alleviate boredom."</p>
-                <p>Direction = -1</p>
-                <p>Impact = 7</p>
-                <p>Total Impact = 1 x -7 = -7</p>
-                <p>Frequency = s</p>
+          <h3>Example 2:</h3>
+          <p>Habit = "Eating to alleviate boredom."</p>
+          <p>Direction = -1</p>
+          <p>Impact = 7</p>
+          <p>Total Impact = 1 x -7 = -7</p>
+          <p>Frequency = s</p>
+        </div>
 
-            </div>
-
-            {/* <div>
+        {/* <div>
 
                 <h1>Data is Powerful</h1>
                 <p>
@@ -112,7 +123,9 @@ export function About(props) {
                 </p>
 
             </div> */}
-        </div>
+      </div>
     </div>
-    );
+  );
 }
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,146 +1,161 @@
-import React, { useState, useEffect } from 'react';
-import {NavBar, CardList, AddCard, ExpandCard} from "./HomePage";
-import SignInPage from './SignInPage';
-import {About} from "./About";
-import {ProfileCard} from "./Profile";
+import React, { useState, useEffect } from "react";
+import { NavBar, CardList, AddCard, ExpandCard, Footer } from "./HomePage";
+import SignInPage from "./SignInPage";
+import About from "./About";
+import { ProfileCard } from "./Profile";
 import CARD_DATA from "../data/cards.json";
-import {Table} from "./MaterialTable";
-import Basic from "./CheckCalendar";
-import {Route, Switch} from 'react-router-dom';
-import { getDatabase, ref, set as firebaseSet, push as firebasePush, onValue } from 'firebase/database';
+import { Route, Switch, Redirect } from "react-router-dom";
+import {
+  getDatabase,
+  ref,
+  set as firebaseSet,
+  push as firebasePush,
+  onValue,
+} from "firebase/database";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-export function App(props) {
-    const [currentCards, setCurrentCards] = useState(CARD_DATA);
-    // const [newHabit, setNewHabit] = useState('');
-    const [cardExpand, setCardExpand] = useState([]);
-    
-    const db = getDatabase(); // not the data; "mailing address"
+function App(props) {
+  const [currentCards, setCurrentCards] = useState(CARD_DATA);
+  // const [newHabit, setNewHabit] = useState('');
+  const [cardExpand, setCardExpand] = useState([]);
 
-    const auth = getAuth();
-    const user = auth.currentUser;
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            
-            // ...
-        } else {
-            // User is signed out
-            // ...
-        }
+  const db = getDatabase(); // not the data; "mailing address"
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
+
+  useEffect(() => {
+    // function when component first loads
+    const habitRef = ref(db, "allHabits");
+    onValue(habitRef, (snapshot) => {
+      const allHabits = snapshot.val(); // extract the value from snapshot
     });
 
-    useEffect(() => { // function when component first loads
-        const habitRef = ref(db, "allHabits");
-        onValue(habitRef, (snapshot) => {
-            const allHabits = snapshot.val(); // extract the value from snapshot
-        });
-
-        // instructions on how to leave
-        // will be called by React 
-        function cleanup() {
-            // turn off the listener
-            console.log("leaving");
-        }
-        return cleanup; // leave instructions behind
-    }, []); // when to re-run (never)
-    // addEventListener('databaseValueChange', () => {})
-    
-
-    const addCard = (cardTitle, cardDescription) => {
-        // update the database
-        const newHabit = {
-            cardTitle: cardTitle,
-            cardText: cardDescription,
-            timestamp: Date.now()
-            // cardImage: "img/wake-up.jpg",
-            // cardImageAlt: "Person waking up"
-        };
-        const habitRef = ref(db, "allHabits/" + user.uid);
-        
-        setCurrentCards([...currentCards, newHabit]);
-        firebaseSet(habitRef, currentCards);
-        
-
-        // const updatedArray = [...currentCards, newCard];
-        // setCurrentCards(updatedArray);
+    // instructions on how to leave
+    // will be called by React
+    function cleanup() {
+      // turn off the listener
+      console.log("leaving");
     }
+    return cleanup; // leave instructions behind
+  }, []); // when to re-run (never)
+  // addEventListener('databaseValueChange', () => {})
 
+  const addCard = (cardTitle, cardDescription) => {
+    // update the database
+    const newHabit = {
+      cardTitle: cardTitle,
+      cardText: cardDescription,
+      timestamp: Date.now(),
+      // cardImage: "img/wake-up.jpg",
+      // cardImageAlt: "Person waking up"
+    };
+    const habitRef = ref(db, "allHabits/" + user.uid);
 
-    const removeCard = (cardDescription) => {
-        let removalIndex = -1
+    setCurrentCards([...currentCards, newHabit]);
+    firebaseSet(habitRef, currentCards);
 
-        let updatedArray = currentCards.map((item, index) => {   
-            if (item.cardText !== cardDescription) {
-                return item;
-            } else {
-                removalIndex = index;
-                return;
-            }
-        });
+    // const updatedArray = [...currentCards, newCard];
+    // setCurrentCards(updatedArray);
+  };
 
-  
-        updatedArray.splice(removalIndex, 1);
-        setCurrentCards(updatedArray);
-    }
+  const removeCard = (cardDescription) => {
+    let removalIndex = -1;
 
+    let updatedArray = currentCards.map((item, index) => {
+      if (item.cardText !== cardDescription) {
+        return item;
+      } else {
+        removalIndex = index;
+        return;
+      }
+    });
 
-    const displaySingleCard = (cardDescription) => {
-        let displayIndex = -1
+    updatedArray.splice(removalIndex, 1);
+    setCurrentCards(updatedArray);
+  };
 
-        let updatedArray = currentCards.map((item, index) => {   
-            if (item.cardText !== cardDescription) {
-                return item;
-            } else {
-                displayIndex = index;
-                return;
-            }
-        });
+  const displaySingleCard = (cardDescription) => {
+    let displayIndex = -1;
 
-        let focus = currentCards[displayIndex];
-        let res = [{
-            "cardTitle": focus.cardTitle,
-            "cardText": focus.cardText,
-            "cardImage": focus.cardImage,
-            "cardImageAlt": focus.cardImageAlt,
-            "impact": focus.impact
-        }];
+    let updatedArray = currentCards.map((item, index) => {
+      if (item.cardText !== cardDescription) {
+        return item;
+      } else {
+        displayIndex = index;
+        return;
+      }
+    });
 
-        setCardExpand(res);
-    }
+    let focus = currentCards[displayIndex];
+    let res = [
+      {
+        cardTitle: focus.cardTitle,
+        cardText: focus.cardText,
+        cardImage: focus.cardImage,
+        cardImageAlt: focus.cardImageAlt,
+        impact: focus.impact,
+      },
+    ];
 
-    const revertToMainCardView = () => {
-        setCardExpand([]);
-    }
+    setCardExpand(res);
+  };
 
+  const revertToMainCardView = () => {
+    setCardExpand([]);
+  };
 
-    // render homepage based on expansion
-    let view;
-    if (cardExpand.length === 0) {
-        view =  [<CardList cardHistory={currentCards} howToRemove={removeCard} singleDisplay={displaySingleCard} key={1}/>,
-                 <AddCard howToAddCard={addCard} key={2} />
-        ] 
-    } else {
-        view = [<ExpandCard card={cardExpand} howToRevert={revertToMainCardView}key={1} />]
-    }
-    
-    return (
+  // render homepage based on expansion
+  let view;
+  if (cardExpand.length === 0) {
+    view = [
+      <CardList
+        cardHistory={currentCards}
+        howToRemove={removeCard}
+        singleDisplay={displaySingleCard}
+        key={1}
+      />,
+      <AddCard howToAddCard={addCard} key={2} />,
+    ];
+  } else {
+    view = [
+      <ExpandCard
+        card={cardExpand}
+        howToRevert={revertToMainCardView}
+        key={1}
+      />,
+    ];
+  }
 
-        <div>
-            <div>
-                <SignInPage />
-            </div>
-            <div>
-                <NavBar />
-                <Switch>
-                    <Route exact path="/"> {view} </Route>
-                    <Route path="/about"> <About /> </Route>
-                    <Route path="/profile"> <ProfileCard /> </Route>
-                </Switch>
-            </div>
-        </div>
-       
-    )
+  return (
+    <div>
+      <NavBar />
 
+      <main className="container">
+        <Switch>
+          <Route exact path="/">
+            <CardList cardHistory={CARD_DATA} />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
+
+export default App;
