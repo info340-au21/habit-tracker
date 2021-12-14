defc6c79a5b03d90f3d149ab6c28cd81485ce6c5
John-Danielsson <johnaugustd@gmail.com>
2021-12-13 20:24:20 -0800

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,9 @@ export default function Footer(props) {
   return (
     <footer id="footer" className="page-footer font-small pt-4" role="content-info" aria-label="footer">
       <div className="container">
-        <p>&copy;Augene Pak, John Danielsson, Andrew Frazier, Luka Marceta</p>
+        <p>&copy; 2021 Augene Pak, John Danielsson, Andrew Frazier, Luka Marceta</p>
         <p>
-          Contact information: ajp824@uw.edu, frazia@uw.edu, lukamarc@uw.edu,
-          johnaugustd@gmail.com
+          Contact information: ajp824@uw.edu, johnd32@uw.edu, frazia@uw.edu, lukamarc@uw.edu
         </p>
       </div>
     </footer>
